00e4fffe08facd59de21d902dab650201e5aacb5
binalajudiya <binalajudiya.ba@gmail.com>
2025-06-03 14:57:58 +0100

    Testing webhook deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -308,7 +308,7 @@ export default function Home({ table, assets }) {
                 link.href = url.toString();
               }
             } catch (e) {
-              // Handlesssaa878908sa invalid URLs ifnmbb needed
+              // Handsasa878908sa invalid URLs ifnmbb needed
             }
           });
         });
